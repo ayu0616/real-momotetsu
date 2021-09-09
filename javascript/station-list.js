9d90c4915f62d7684c9318f6a9f8a596d79beff2
ayu0616 <83691477+ayu0616@users.noreply.github.com>
2021-09-09 17:30:36 +0900

    Update station-list.js

--- javascript/station-list.js
+++ javascript/station-list.js
@@ -1,12 +1,16 @@
-const createTable = () => {
-    const tbody = $('#tbody');
-    let tr = $('<tr></tr>');
-    let td1 = $('<td></td>');
-    let td2 = $('<td></td>');
-    let td3 = $('<td></td>');
-    let td4 = $('<td></td>');
-    tr.append(td1, td2, td3, td4)
-    tbody.append(tr)
-}
+// const createTable = () => {
+//     const tbody = $('#tbody');
+//     let tr = $('<tr></tr>');
+//     let td1 = $('<td></td>');
+//     let td2 = $('<td></td>');
+//     let td3 = $('<td></td>');
+//     let td4 = $('<td></td>');
+//     tr.append(td1, td2, td3, td4)
+//     tbody.append(tr)
+// }
 
-createTable()
+// createTable()
+
+$(function(){
+    $('#view1').csv2table('/山陽本線（岡山→宮島口）.csv');
+    });
