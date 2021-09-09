bf7a376411497190436d79cfcb717a2af2d75f23
ayu0616 <83691477+ayu0616@users.noreply.github.com>
2021-09-09 17:54:39 +0900

    Update station-list.js

--- javascript/station-list.js
+++ javascript/station-list.js
@@ -1,12 +1,29 @@
 const createTable = () => {
     const tbody = $('#tbody');
-    let tr = $('<tr></tr>');
-    let td1 = $('<td></td>');
-    let td2 = $('<td></td>');
-    let td3 = $('<td></td>');
-    let td4 = $('<td></td>');
-    tr.append(td1, td2, td3, td4)
-    tbody.append(tr)
+    $.getJSON("山陽本線（岡山→宮島口）.json", function(json) {
+        const jsonNumber = json.Number
+        const jsonStation = json.駅
+        const jsonForced = json.必ず下車
+        const jsonMission = json.ミッション
+        const len = Object.keys(jsonNumber).length
+        for(i = 0; i <= len; i ++) {
+            let tr = $('<tr></tr>');
+            let td1 = $('<td></td>', {
+                text:jsonNumber[i]
+            });
+            let td2 = $('<td></td>', {
+                text:jsonStation[i]
+            });
+            let td3 = $('<td></td>', {
+                text:jsonForced[i]
+            });
+            let td4 = $('<td></td>', {
+                text:jsonMission[i]
+            });
+            tr.append(td1, td2, td3, td4)
+            tbody.append(tr)
+        }
+    });
 }
 
 createTable()
