8a4b00117021ecb65d7f72cefc599fc95075a659
ayu0616 <83691477+ayu0616@users.noreply.github.com>
2021-09-11 21:38:35 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 時計を表示する関数を定義
 const showClock = () => {
     let dateTime1 = new Date();
     let date = dateTime1.getFullYear() + "年" +
@@ -12,5 +13,28 @@ const showClock = () => {
     $('#now-time').text(time);
 }
 
+// ナビバーのリスト作成する関数を定義
+const navList = (text, url) => {
+    const li = $('<li></li>', {
+        'class': 'nav-item',
+    });
+    const a = $('<a></a>', {
+        text: text,
+        href: url,
+        'class': "nav-link active text-decoration-underline",
+        'aria-current': "page"
+    });
+    return li.append(a)
+}
+
+// ナビバーのリストを表示する関数を定義
+const showNav = () => {
+    const li1 = navList('TOP PAGE', 'index.html')
+    const li2 = navList('STATION LIST', 'station-list.html')
+    const li3 = navList('DICE', 'dice.html')
+    $('#navbar-nav').append(li1, li2, li3)
+}
+
 showClock()
 setInterval('showClock()',1000);
+showNav()
