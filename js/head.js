cec3dad4c908f5f58f6da40c76ace4212930e7a0
ayu0616 <ayumuogawa0616@gmail.com>
2021-10-09 14:58:26 +0900

    add head.js

---
+++ js/head.js
@@ -0,0 +1,17 @@
+const head = `
+<meta name="google-site-verification" content="NBVvw2Fil32wK5Bqv59_hcUBzD6vtejIoTVzEb9J5q8" />
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="author" content="ayu0616">
+<!-- アイコン読み込み -->
+<link rel="icon" href="./images/peach.png">
+<link rel="apple-touch-icon" href="./images/peach.jpg">
+<!-- BootstrapのCSS読み込み -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+<!-- 自分のCSS読み込み -->
+<link rel="stylesheet" href="./css/main.css">
+`;
+
+$('head').prepend(head)
